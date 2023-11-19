b1c3fb1b38ef969151b1b6dea595cab916230757
Vinicius Granado <vinicius.r.granado@gmail.com>
2023-11-19 14:11:17 -0300

    MongoDB configurarion

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+require('dotenv').config();
 
 const app = express();
 
@@ -20,3 +23,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/")
 });
+
+const MONGO_URL = `mongodb+srv://viniciusrgranado:${process.env.MONGO_PASSWORD}@cluster0.xmpbagx.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
