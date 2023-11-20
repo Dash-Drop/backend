275efadb8d558ff45226b4b245f0a02a17ffc4f6
Vinicius Granado <vinicius.r.granado@gmail.com>
2023-11-20 14:17:08 -0300

    Create File model

---
+++ src/db/files.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const FileSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  path: { type: String, required: true }
+});
+
+export const FileModel = mongoose.model('File', FileSchema);
+
+export const getFiles = () => FileModel.find();
+export const getFileById = (id: string) => FileModel.findById(id);
+export const createFile = (values: Record<string, any>) => new FileModel(values).save().then((file) => file.toObject());
+export const deleteFileById = (id: string) => FileModel.findOneAndDelete({ id: id });
